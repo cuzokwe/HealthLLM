582406248e9fd53c334d7c3ab9b0e9e449b4eccb
cuzokwe <chrisuzokwe@zeroeyes.com>
2024-06-11 12:31:20 -0400

    changed streamlit calls to search from vectorstore

--- home.py
+++ home.py
@@ -1,13 +1,25 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain.chains import RetrievalQA
+from pinecone import Pinecone, ServerlessSpec
+
+from docs import load_documents
+import os
 
 st.title('🦜🔗 Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(input_text):
+
+    vectorstore = PineconeVectorStore(index_name='health-research-index')
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever())
+    st.info(qa_chain.run(input_text))
+
+
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
